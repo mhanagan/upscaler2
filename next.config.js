885fdb6761e5d2506d9e6d3ea07989c53d1a647e
Matt <mattew.hanagan@gmail.com>
2024-11-25 00:27:23 -0500

    build error fix

--- next.config.js
+++ next.config.js
@@ -11,6 +11,11 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'replicate.delivery',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        pathname: '/**',
       }
     ],
   },
